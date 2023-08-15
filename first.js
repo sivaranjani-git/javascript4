8a066ba853ec2adf8501f8e344676987f745d17a
sivaranjani-git <134845313+sivaranjani-git@users.noreply.github.com>
2023-08-15 13:03:33 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,18 @@
+//EXPLICIT CONVERSION
+
+let value="123";
+let value2=Number(value);
+console.log(value2);
+console.log(value2 + 1);
+
+let bool=true;
+let result= Number(false);
+console.log(result);
+
+let final = Number(null);
+console.log(final);
+
+let empty=" ";
+console.log(Number(empty));
+
+//number(),parseint(),parseout()
